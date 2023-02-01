5260bd3912bb5382d9448a497a5a41f594820166
cagdas <cagdasazgin@gmail.com>
2023-01-31 17:20:28 -0800

    i added some changes math operation

--- src/Day1GitHub/FirstGitHubClass.java
+++ src/Day1GitHub/FirstGitHubClass.java
@@ -2,10 +2,7 @@ package Day1GitHub;
 
 public class FirstGitHubClass {
     public static void main(String[] args) {
-
         System.out.println("Hey GitHub");
         System.out.println(1+1);
-
-
     }
 }
